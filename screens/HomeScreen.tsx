4147dbd850357d8e19131c5e0e7b49e2107d1778
Lucas Lee <lucas1203lee@gmail.com>
2023-03-03 15:25:39 -0500

    TabController + GridList

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,8 +1,47 @@
-import {Button, Colors, Incubator, TabController, Text, View} from "react-native-ui-lib";
+import {
+    BorderRadiuses,
+    Button,
+    Card,
+    Colors, GridList,
+    Incubator,
+    Spacings,
+    TabController,
+    Text,
+    View
+} from "react-native-ui-lib";
 import {StyleSheet, TextStyle} from "react-native";
 import {RootTabScreenProps} from "../types";
-import {SafeAreaView} from "react-native-safe-area-context";
-import {BottomTabScreenProps} from "@react-navigation/bottom-tabs";
+
+const data = [1, 2, 3, 4];
+// @ts-ignore
+function renderItem({ item }) {
+    return (
+        <Card
+            height={120}
+            flex1
+            marginL-10
+            marginR-10
+        >
+            <Card.Section
+                content={[{text: 'Card content here', text70: true, red30: true}]}
+                contentStyle={{alignItems: 'center'}}
+            />
+            <Card.Section
+                content={[{text: item.toString(), text90BL: true, red30: true}]}
+                contentStyle={{alignItems: 'center'}}
+            />
+        </Card>
+    );
+}
+export function renderExplorePage() {
+    return (
+        <GridList data={data}
+                  renderItem={renderItem}
+                  numColumns={1}
+                  contentContainerStyle={styles.list}
+        />
+    )
+}
 
 export default function HomeScreen({navigation}: RootTabScreenProps<'Home'>) {
 
@@ -30,11 +69,12 @@ export default function HomeScreen({navigation}: RootTabScreenProps<'Home'>) {
            selectedLabelStyle={tabTextStyle}
            selectedLabelColor="#000"
          />
-         <View>
+         <View flex>
            <TabController.TabPage index={0}>
-             <View flex style={{height: "100%"}}>
-               <Text>0</Text>
-             </View>
+               {renderExplorePage()}
+             {/*<View flex style={{height: "100%"}}>*/}
+             {/*    <Text>0</Text>*/}
+             {/*</View>*/}
            </TabController.TabPage>
            <TabController.TabPage index={1} lazy>
              <Text>1</Text>
@@ -44,3 +84,13 @@ export default function HomeScreen({navigation}: RootTabScreenProps<'Home'>) {
      </View>
   );
 }
+const styles = StyleSheet.create({
+    list: {
+        paddingTop: Spacings.s5,
+    },
+    itemImage: {
+        width: '100%',
+        height: 85,
+        borderRadius: BorderRadiuses.br10
+    }
+});
