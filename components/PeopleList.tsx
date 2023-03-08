b60d8fd2196009168058c2203c9994d5f9089435
lacrimosaofdana <lacrimosaofdana@hotmail.com>
2023-03-08 18:03:29 -0500

    Trip Details PeopleList Update

--- components/PeopleList.tsx
+++ components/PeopleList.tsx
@@ -1,14 +1,33 @@
-import {Colors, GridList, Spacings, Text, TextField, View} from "react-native-ui-lib";
+import {
+    Avatar,
+    BorderRadiuses,
+    Button,
+    Card,
+    Colors,
+    GridList,
+    Spacings,
+    Text,
+    TextField,
+    View
+} from "react-native-ui-lib";
+import {StyleSheet} from 'react-native';
 import {User} from "../Interface/TripInterface"
+import React from "react";
 
 export default function PeopleList(props: {people: User[]}) {
   return (
     <GridList
-      data={props.people}
-      renderItem={({item}) => renderItem(item)}
-      numColumns={1}
-      itemSpacing={Spacings.s2}
-      listPadding={Spacings.s2}
+        ListHeaderComponent={
+            <Text h1 marginB-s5>
+                Co-passengers
+            </Text>
+        }
+        data={props.people}
+        renderItem={({item}) => renderItem(item)}
+        numColumns={1}
+        itemSpacing={Spacings.s2}
+        listPadding={Spacings.s2}
+        contentContainerStyle={styles.list}
     >
     </GridList>
   );
@@ -16,8 +35,42 @@ export default function PeopleList(props: {people: User[]}) {
 
 function renderItem(item: User) {
   return (
-    <View>
-
-    </View>
+      <Card style={{flex:1,
+      flexDirection: 'row',
+      height:60}}>
+          <View flex-1 paddingL-15 paddingT-15>
+              {
+                  <Avatar size={30}
+                          name={item.firstName}
+                          backgroundColor={Colors.$backgroundWarningLight}
+                          labelColor={Colors.$textMajor}
+                          containerStyle={{marginLeft: 0}}
+                  ></Avatar>
+              }
+          </View>
+          <View flex-2 paddingT-10>
+              <View flex-1>
+                  <Text $textDefault>{item.firstName+" "+item.lastName}</Text>
+              </View>
+              <View flex-1>
+                  <Text $textDefault>{"Rate: 4.5"}</Text>
+              </View>
+          </View>
+          <View flex-3 paddingT-10>
+              <Text $textDefault>{"Group Creator"}</Text>
+          </View>
+          <View flex-1></View>
+      </Card>
   );
 }
+
+const styles = StyleSheet.create({
+    list: {
+        paddingTop: Spacings.s5
+    },
+    itemImage: {
+        width: '100%',
+        height: 85,
+        borderRadius: BorderRadiuses.br10
+    }
+});
--- screens/TripDetailsScreen.tsx
+++ screens/TripDetailsScreen.tsx
@@ -1,15 +1,39 @@
 import {Button, Colors, Incubator, Text, View} from "react-native-ui-lib";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Image } from "react-native";
 import {RootStackParamList, RootStackScreenProps, RootTabScreenProps} from "../types";
 import {SafeAreaView} from "react-native-safe-area-context";
 import {trips} from "../assets/data/dummyData";
+import PeopleList from "../components/PeopleList";
+import renderItem from "../components/PeopleList";
+import {Trip} from "../Interface/TripInterface";
+import {User} from "../Interface/TripInterface"
+import TripCard from "../components/TripCard";
+import React from "react";
 
 export default function TripDetailsScreen({route, navigation}: RootStackScreenProps<'TripDetails'>) {
     const tripId = route.params?.id;
     return (
         <View useSafeArea>
-            <Text> Home Screen </Text>
-            <Text> {JSON.stringify(trips[tripId as number])} </Text>
+            <PeopleList people={trips[tripId as number].riders as User[]}></PeopleList>
+            <View centerH paddingT-20>
+                <Button
+                    label={'Join'}
+                    size={Button.sizes.medium}
+                    backgroundColor={Colors.red30}
+                    labelStyle={{fontWeight: '500', letterSpacing: -0.5}}
+                    style={{
+                        width: 100,
+                        height: 50,
+                    }}
+                />
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    image: {
+        width: 200,
+        height: 200,
+    },
+})
