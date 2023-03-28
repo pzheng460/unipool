30bf0c980916594dbc6597242ccb0a6579f83245
Lucas Lee <lucas1203lee@gmail.com>
2023-03-28 17:03:49 -0400

    update dummy data

--- Interface/TripInterface.ts
+++ Interface/TripInterface.ts
@@ -1,23 +1,32 @@
 export interface Trip {
   id: number;
-  from?: string;
-  to?: string;
-  roundTrip?: boolean;
-  date?: number;
+  from: string;
+  to: string;
+  roundTrip: boolean;
+  date: number;
   returnDate?: number;
-  seatsTaken?: number;
-  seatsMax?: number;
-  riders?: User[];
-  sameGender?: boolean;
+  seatsTaken: number;
+  seatsMax: number;
+  riders: User[];
+  sameGender: boolean;
 }
 
 export interface User {
   id: number;
-  firstName?: string;
-  lastName?: string;
-  phone?: string;
-  email?: string;
-  eduEmail?: string;
-  eduVerified?: boolean;
-  gender?: string;
+  firstName: string;
+  lastName: string;
+
+  avatar?: string;
+  phone: string;
+  email: string;
+  eduEmail: string;
+  eduVerified: boolean;
+  gender: string;
+
+  pastTrips: Trip[];
+  upcomingTrips: Trip[];
+
+  rating: number;
+  numOfRatings: number;
+
 }
--- assets/data/dummyData.ts
+++ assets/data/dummyData.ts
@@ -1,25 +1,71 @@
 import {Trip, User} from "../../Interface/TripInterface";
 
-const user1 : User = {
+export const user1 : User = {
     id: 0,
     firstName: 'Lucas',
     lastName: 'Lee',
+    avatar: undefined,
     phone: '7702868930',
     email: 'lucas1203lee@gmail.com',
     eduEmail: 'hli427@gatech.edu',
     eduVerified: true,
     gender: 'male',
+
+    pastTrips: [],
+    upcomingTrips:[],
+    rating: 4.67,
+    numOfRatings: 3,
 };
 
-const user2 : User = {
+export const user2 : User = {
     id: 1,
     firstName: 'Ruiqi',
     lastName: 'Liu',
+    avatar: undefined,
     phone: '4706767789',
     email: 'ruiqiliusysu@gmail.com',
     eduEmail: 'rliu425@gatech.edu',
     eduVerified: true,
     gender: 'male',
+
+    pastTrips: [],
+    upcomingTrips:[],
+    rating: 5,
+    numOfRatings: 0,
+};
+
+export const user3: User = {
+    id: 2,
+    firstName: 'John',
+    lastName: 'Hill',
+    avatar: undefined,
+    phone: '7702868000',
+    email: 'johnhill@gmail.com',
+    eduEmail: 'jhill817@gatech.edu',
+    eduVerified: true,
+    gender: 'male',
+
+    pastTrips: [],
+    upcomingTrips:[],
+    rating: 5,
+    numOfRatings: 1,
+};
+
+export const user4: User = {
+    id: 3,
+    firstName: 'Jaden',
+    lastName: 'Bryan',
+    avatar: undefined,
+    phone: '4002866856',
+    email: 'jadenbryan@gmail.com',
+    eduEmail: 'jbryan279@gatech.edu',
+    eduVerified: true,
+    gender: 'male',
+
+    pastTrips: [],
+    upcomingTrips:[],
+    rating: 5,
+    numOfRatings: 1,
 };
 
 export const trip1: Trip = {
@@ -37,7 +83,7 @@ export const trip1: Trip = {
     sameGender: false,
 };
 
-const trip2: Trip = {
+export const trip2: Trip = {
     id: 1,
     from: "Georgia Tech",
     to: "H Mart",
@@ -52,7 +98,7 @@ const trip2: Trip = {
     sameGender: true,
 };
 
-const trip3: Trip = {
+export const trip3: Trip = {
     id: 2,
     from: "Georgia Tech",
     to: "Regal Atlantic Station",
@@ -67,6 +113,55 @@ const trip3: Trip = {
     sameGender: false,
 };
 
-export const users : User[] = [user1, user2];
+export const pastTrip1: Trip = {
+    id: 3,
+    from: "Hartsfield-Jackson Atlanta International Airport",
+    to: "Georgia Tech",
+    roundTrip: false,
+    date: 30000,
+    returnDate: undefined,
 
+    seatsTaken: 3,
+    seatsMax: 4,
+    riders: [user1, user3, user4],
+
+    sameGender: false,
+};
+
+export const pastTrip2: Trip = {
+    id: 4,
+    from: "H Mart",
+    to: "Georgia Tech",
+    roundTrip: false,
+    date: 40000,
+    returnDate: undefined,
+
+    seatsTaken: 3,
+    seatsMax: 4,
+    riders: [user1, user3, user4],
+
+    sameGender: true,
+};
+
+export const pastTrip3: Trip = {
+    id: 5,
+    from: "Regal Atlantic Station",
+    to: "Georgia Tech",
+    roundTrip: false,
+    date: 50000,
+    returnDate: undefined,
+
+    seatsTaken: 3,
+    seatsMax: 4,
+    riders: [user1, user3, user4],
+
+    sameGender: false,
+};
+
+user1.pastTrips = [...user3.pastTrips, pastTrip1, pastTrip2, pastTrip3];
+user3.pastTrips = [...user3.pastTrips, pastTrip1, pastTrip2, pastTrip3];
+user4.pastTrips = [...user4.pastTrips, pastTrip1, pastTrip2, pastTrip3];
+
+export const users : User[] = [user1, user2, user3, user4];
 export const trips : Trip[] = [trip1, trip2, trip3];
+export const pastTrips: Trip[] = [pastTrip1, pastTrip2, pastTrip3];
