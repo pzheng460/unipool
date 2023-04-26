99ad6cb6d285e19cbf13e41a389f290349cd4256
lacrimosaofdana <lacrimosaofdana@hotmail.com>
2023-04-26 03:18:28 -0400

    Recommender Update

--- recommender/recommender.py
+++ recommender/recommender.py
@@ -4,16 +4,10 @@ from firebase_admin import firestore
 import pandas as pd
 from geopy import distance
 import datetime
+import tkinter as tk
+from tkinter import messagebox
 
-cred = credentials.Certificate('iungo-8f30b-firebase-adminsdk-7obwd-898d3a1aee.json')
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
-
-expected_columns1 = ['toCoordinates', 'roundTrip', 'seatsTaken', 'returnDate', 'seatsMax', 'type', 'date', 'from', 'to', 'riders', 'sameGender', 'fromCoordinates']
-expected_columns2 = ['numOfRatings', 'lastName', 'firstName', 'comments', 'rating', 'pastTrips', 'gender', 'upcomingTrips', 'coordinates', 'email']
-
-def read_data(collection_name, expected_columns):
+def read_data(db, collection_name, expected_columns):
     ref = db.collection(collection_name)
     docs = ref.get()
 
@@ -38,43 +32,69 @@ def read_data(collection_name, expected_columns):
     df.dropna()
     return df
 
-# Read data from the database
-df_trips = read_data(u'trips', expected_columns1)
+class App:
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry("400x200")
+        master.title("Recommender")
 
-df_users = read_data(u'users', expected_columns2)
+        self.label = tk.Label(master, text="Generate the recommendations for our users:")
+        self.label.pack()
 
+        self.button = tk.Button(master, text="Start", height=2, width=10, command=self.run_program)
+        self.button.pack(pady=50, padx=(master.winfo_width() - 100) // 2)
 
+    def run_program(self):
+        try:
+            cred = credentials.Certificate('iungo-8f30b-firebase-adminsdk-7obwd-898d3a1aee.json')
+            firebase_admin.initialize_app(cred)
 
-users_coord = df_users['coordinates']
+            db = firestore.client()
 
-total_rec = {}
-for index, row in df_users.iterrows():
-    user_coord = row['coordinates']
-    trips_coord = df_trips['fromCoordinates']
-    dis = []
-    for i in range(len(trips_coord)):
-        dis.append(distance.distance(user_coord, trips_coord[i]).km)
-    df_trips['dis_users_trips'] = dis
+            expected_columns1 = ['toCoordinates', 'roundTrip', 'seatsTaken', 'returnDate', 'seatsMax', 'type', 'date',
+                                 'from', 'to', 'riders', 'sameGender', 'fromCoordinates']
+            expected_columns2 = ['numOfRatings', 'lastName', 'firstName', 'comments', 'rating', 'pastTrips', 'gender',
+                                 'upcomingTrips', 'coordinates', 'email']
 
-    now = datetime.datetime.now()
-    delta_time = []
-    for time in df_trips['date']:
-        dt_seconds = (datetime.datetime.fromtimestamp(time) - now).total_seconds()
-        if dt_seconds < 0:
-            dt_seconds = 1e9
-        delta_time.append(dt_seconds)
+            # Read data from the database
+            df_trips = read_data(db, u'trips', expected_columns1)
+            df_users = read_data(db, u'users', expected_columns2)
 
-    df_trips['delta_time'] = delta_time
+            total_rec = {}
+            for index, row in df_users.iterrows():
+                user_coord = row['coordinates']
+                trips_coord = df_trips['fromCoordinates']
+                dis = []
+                for i in range(len(trips_coord)):
+                    dis.append(distance.distance(user_coord, trips_coord[i]).km)
+                df_trips['dis_users_trips'] = dis
 
-    df_trips['sort_value'] = df_trips['dis_users_trips'] * 1e4 + df_trips['delta_time']
+                now = datetime.datetime.now()
+                delta_time = []
+                for time in df_trips['date']:
+                    dt_seconds = (datetime.datetime.fromtimestamp(time) - now).total_seconds()
+                    if dt_seconds < 0:
+                        dt_seconds = 1e9
+                    delta_time.append(dt_seconds)
 
-    df_sorted = df_trips.sort_values('sort_value', ascending=True).head(5)
+                df_trips['delta_time'] = delta_time
 
-    df_sorted.drop(['dis_users_trips', 'delta_time', 'sort_value'], axis=1, inplace=True)
-    print(df_sorted)
-    rec = df_sorted.index.tolist()
-    total_rec[index] = rec
-# Write data to the database
-rec_ref = db.collection(u'recommendations').document('test')
+                df_trips['sort_value'] = df_trips['dis_users_trips'] * 1e4 + df_trips['delta_time']
 
-rec_ref.set(total_rec)
+                df_sorted = df_trips.sort_values('sort_value', ascending=True).head(5)
+
+                df_sorted.drop(['dis_users_trips', 'delta_time', 'sort_value'], axis=1, inplace=True)
+                #print(df_sorted)
+                rec = df_sorted.index.tolist()
+                total_rec[index] = rec
+            # Write data to the database
+            rec_ref = db.collection(u'recommendations').document('test')
+            rec_ref.set(total_rec)
+
+            messagebox.showinfo("Program Complete", "The program has finished running!")
+        except Exception as e:
+            messagebox.showerror("Error", str(e))
+
+root = tk.Tk()
+app = App(root)
+root.mainloop()
